faf8123cdd1d7580c11c2177eed532cfb58fc078
Agrim Gupta <agrimg8@gmail.com>
2025-09-09 16:28:51 +0530

    DAY04

--- Day04.cpp
+++ Day04.cpp
@@ -42,6 +42,22 @@ int main() {
     // }
 
      // Given an integer add a having N element. Find the number of occurrence of 1, and remove the element add the index, which is equal to the number of occurrence of 1. Print the modified array given that count of 1 is less than N
+    // int n;
+    // cin>>n;
+    // vector<int> arr;
+    // for(int i=0; i<n; i++){
+    //     int x;
+    //     cin>>x;
+    //     arr.push_back(x);
+    // }
+    // int cnt = count(arr.begin(), arr.end(),1);
+    // cout<<cnt;
+    // arr.erase(arr.begin()+cnt);
+    // for(int i=0; i<n; i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    
+     // given an integer having an element first find the first location of minimum and maximum element split the array into three parts from these locations build another array by concatenating the second part followed by the first part followed by the third part print the elements of this array in a single line it can be assume that the minimum and maximum elements will not be the same.
     int n;
     cin>>n;
     vector<int> arr;
@@ -50,11 +66,29 @@ int main() {
         cin>>x;
         arr.push_back(x);
     }
-    int cnt = count(arr.begin(), arr.end(),1);
-    cout<<cnt;
-    arr.erase(arr.begin()+cnt);
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
+    int mini = *min_element(arr.begin(), arr.end());
+    int maxi = *max_element(arr.begin(), arr.end());
     
+    auto mini1 = find(arr.begin(),arr.end(), mini)-arr.begin();
+    auto maxi1 = find(arr.begin(), arr.end(), maxi)-arr.begin();
+    vector<int> ans;
+    if(mini1<maxi1){
+        for(int i=mini1; i<maxi1; i++)
+            ans.push_back(arr[i]);
+        for(int i=0; i<mini1; i++)
+            ans.push_back(arr[i]);
+        for(int i=maxi1; i<n; i++)
+            ans.push_back(arr[i]);
+    }
+    else{
+        for(int i=maxi1; i<mini1; i++)
+            ans.push_back(arr[i]);
+        for(int i=0; i<maxi1; i++)
+            ans.push_back(arr[i]);
+        for(int i=mini1; i<n; i++)
+            ans.push_back(arr[i]);
+    }
+    for(int i=0; i<n; i++){
+        cout<<ans[i]<<" ";
+    }
 }
