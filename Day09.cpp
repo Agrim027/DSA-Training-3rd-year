1c1d0660503ec90bb46d693d3d98e73cbb931566
Agrim Gupta <agrimg8@gmail.com>
2025-09-30 16:27:19 +0530

    Day09

--- Day09.cpp
+++ Day09.cpp
@@ -28,21 +28,65 @@ int main() {
     
 
     //Replace  all the small case letters with the corresponding capital case. Replace all capital case letters with the A and replace all the digits with the letter B
-    string str;
-    cin>>str;
-    string ans = "";
-    for(int i=0; i<str.length(); i++){
-        if(islower(str[i])){
-            ans += (char)toupper(str[i]);
+    // string str;
+    // cin>>str;
+    // string ans = "";
+    // for(int i=0; i<str.length(); i++){
+    //     if(islower(str[i])){
+    //         ans += (char)toupper(str[i]);
+    //     }
+    //     else if(isupper(str[i])){
+    //         ans += 'A';
+    //     }
+    //     else if(isdigit(str[i])){
+    //         ans += 'B';
+    //     }
+    // }
+    // cout<<ans;
+
+
+    // 
+    // int n;                                          //9
+    // cin>>n;
+    // string s;                                     //2ab3cd45e
+    // cin>>s;
+    // int sum;
+    // string str = "";
+    // for(int i=0; i<n; i++){
+    //     if(isdigit(s[i]))
+    //         sum += (int)(s[i]);
+            
+    //     else{
+    //         str += s[i];
+    //     }
+        
+    // }
+    // sum = sum % n;
+    // if(sum%2==0){
+    //     cout<< str.substr(n-sum) + str.substr(0, n-sum);  //right rotate
+    // }
+    // else{
+    //     cout<< str.substr(sum) + str.substr(0,sum);  //left rotate;
+    // }
+
+    //count the number of characters between the character and its next occurence. if no next occurence is found return -1
+    int n;
+    cin>>n;                                // 7
+    string s;                              //Ababaad
+    cin >> s;
+
+    for(char &c : s) c = tolower(c);
+
+    for(int i = 0; i < n; i++){
+        int gap = -1;
+        for(int j = i + 1; j < n; j++){
+            if (s[j] == s[i]) {
+                gap = j - i - 1;
+                break;
+            }
         }
-        else if(isupper(str[i])){
-            ans += 'A';
-        }
-        else if(isdigit(str[i])){
-            ans += 'B';
-        }
+        cout<<gap;
     }
-    cout<<ans;
     
     
 }
